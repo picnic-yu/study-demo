c259a19347d50df562cd61536f090b4a16bf5654
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-11-29 15:23:55 +0800

    手写express 中间件封装

--- node/express/express/index.js
+++ node/express/express/index.js
@@ -7,16 +7,53 @@ function createApplication(){
         let m = req.method.toLowerCase();
         // url.parse()可以将一个完整的URL地址，分为很多部分，常用的有：host、port、pathname、path、query。
         const {pathname} = url.parse(req.url,true);
-        // 服务器监听，当匹配到对应得路由 执行回调函数
-        for(let i =0;i<app.routes.length;i++){
-            let route = app.routes[i];
-            let {method,path,handler} =  app.routes[i];
-            // all 和 * 是处理匹配所有方法和所有路径
-            if((method === m || method==='all')&& (path === pathname || path==='*')){
-                return handler(req,res);
+        // 通过next迭代
+        let index = 0;
+        function next (err) {
+            if(index === app.routes.length){
+                return res.end('not')
             }
+            let {method,path,handler} = app.routes[index++]//每次调用next就应该调用下一个
+            if(err){
+                // 如果有错误 应该去找错误中间件 
+                // 错误中间件回调函数有4哥参数
+                // 判断函数参数个数 hanshu.length
+                if(handler.length === 4){
+                    handler(err,req,res,next)
+                }else{
+                    next(err)
+                }
+            }else{
+               
+                if(method === 'middle'){
+                    // 处理中间件
+                    if(path== '/' || path === pathname || pathname.startsWith(path+'/')){
+                        handler(req,res,next);
+                    }else{
+                        next();
+                    }
+                }else{
+                    // 处理路由
+                    if((method === m || method==='all')&& (path === pathname || path==='*')){
+                        return handler(req,res);
+                    }else{
+                        next();
+                    }
+                }
+            }
+            
         }
-        res.end('not');
+        next();//中间件next
+        // 服务器监听，当匹配到对应得路由 执行回调函数
+        // for(let i =0;i<app.routes.length;i++){
+        //     let route = app.routes[i];
+        //     let {method,path,handler} =  app.routes[i];
+        //     // all 和 * 是处理匹配所有方法和所有路径
+        //     if((method === m || method==='all')&& (path === pathname || path==='*')){
+        //         return handler(req,res);
+        //     }
+        // }
+        // res.end('not');
     }
     http.METHODS.forEach((method) => {
         method = method.toLocaleLowerCase();//小写
@@ -30,6 +67,19 @@ function createApplication(){
     });
     
     app.routes = [];
+    // 中间件
+    app.use = function(path,handler){
+        if(typeof handler !== 'function'){
+            handler = path;
+            path = '/';
+        }
+        app.routes.push({
+            method:'middle',//中间件
+            path,
+            handler
+        })
+
+    }
     app.all = function(path,handler){
         app.routes.push({
             method:'all',//匹配全部
